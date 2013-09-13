56615565ce6fddde0fcef03c35a1e410614a91ae
Federico Tomassetti <f.tomassetti@gmail.com>
2013-09-13 14:42:24 +0200

    Polishing the metamodel...

--- lib/js-lightmodels/metamodel.rb
+++ lib/js-lightmodels/metamodel.rb
@@ -12,7 +12,6 @@ module Js
 	JavaBoolean   = ::Java::boolean.java_class
 	JavaInt 	  = ::Java::int.java_class
 	JavaDouble 	  = ::Java::double.java_class
-	JavaArray 	  = ::Java::int.java_class
 	JavaCollectionTypes = [JavaList,JavaSortedSet]
 
 	MappedAstClasses = {}
@@ -24,18 +23,20 @@ module Js
 	end
 
 	def self.is_base_class?(name)
-		['org.mozilla.javascript.Node','org.mozilla.javascript.ast.AstNode'].include? name
+		# Object is here because of Symbol, which does not extenf Node
+		['org.mozilla.javascript.Node','org.mozilla.javascript.ast.AstNode','java.lang.Object'].include? name
 	end
 
-	def self.get_att_type(type_name)
-		case type_name
-		when JavaString.name
+	def self.get_att_type(type)
+		return String if type.enum?
+		case type
+		when JavaString
 			String
-		when JavaBoolean.name
+		when JavaBoolean
 			RGen::MetamodelBuilder::DataTypes::Boolean
-		when JavaInt.name
+		when JavaInt
 			Integer
-		when JavaDouble.name
+		when JavaDouble
 			Float				
 		else
 			nil
@@ -63,35 +64,31 @@ module Js
 	end
 
 	def self.metasuperclass(java_super_class)
+		if is_base_class?(java_super_class.name)				
+			RGen::MetamodelBuilder::MMBase
+		else
+			raise "Super class #{java_super_class.name} of #{java_class.name}. It should be wrapped before this class!" unless MappedAstClasses[java_super_class]
+			MappedAstClasses[java_super_class]
+		end
 	end
 
 	def self.wrap(ast_names)		
 		# first create all the classes
 		ast_names.each do |ast_name|
-			java_class       = rhino_node_class(ast_name)
-			java_super_class = java_class.superclass
-			#super_class      = get_metaclass_by_name(java_super_class.name)
-			if java_super_class.name == 'org.mozilla.javascript.ast.AstNode'
-				super_class = RGen::MetamodelBuilder::MMBase
-			elsif java_super_class.name == 'java.lang.Object'
-				super_class = RGen::MetamodelBuilder::MMBase
-			else
-				raise "Super class #{java_super_class.name} of #{java_class.name}. It should be wrapped before!" unless MappedAstClasses[java_super_class]
-				super_class = MappedAstClasses[java_super_class]
-			end
-			#puts "Java Super Class: #{java_super_class.name}"
-			ast_class = java_class.ruby_class
-			#puts "Class #{simple_java_class_name(ast_class)} extends #{super_class}"
-			c = Class.new(super_class)
-			raise "Already mapped! #{ast_name}" if MappedAstClasses[java_class]
-			MappedAstClasses[java_class] = c
-			Js.const_set simple_java_class_name(ast_class), c
+			ast_java_class   = rhino_node_class(ast_name)
+			meta_super_class = metasuperclass(ast_java_class.superclass)			
+			meta_class       = Class.new(meta_super_class)
+			
+			raise "Already mapped! #{ast_name}" if MappedAstClasses[ast_java_class]
+			MappedAstClasses[ast_java_class] = meta_class
+			
+			Js.const_set ast_java_class.simple_name, meta_class
 		end
 
 		# then add all the properties and attributes
 		ast_names.each do |ast_name|
-			java_class = ::Java::JavaClass.for_name("org.mozilla.javascript.ast.#{ast_name}")
-			ast_class = java_class.ruby_class
+			java_class = rhino_node_class(ast_name)
+			ast_class  = java_class.ruby_class
 			c = MappedAstClasses[java_class]
 				
 			to_ignore = %w( symbolTable compilerData comments liveLocals regexpString
@@ -108,18 +105,18 @@ module Js
 				ast_class.java_class.declared_instance_methods.select { |m| Js.getter?(m) }.each do |m|
 					prop_name = LightModels::Js.property_name(m)
 					unless to_ignore.include?(prop_name)
-						if Js.get_att_type(m.return_type.name)
-							has_attr prop_name, Js.get_att_type(m.return_type.name)
+						if Js.get_att_type(m.return_type)
+							# the type is simple (-> attribute)
+							has_attr prop_name, Js.get_att_type(m.return_type)
 						elsif MappedAstClasses.has_key?(m.return_type)
+							# the type is complex (-> reference)
 							contains_one_uni prop_name, MappedAstClasses[m.return_type]
-						elsif JavaCollectionTypes.include?(m.return_type)
+						elsif JavaCollectionTypes.include?(m.return_type)							
 							type_name = LightModels::Js.get_generic_param_name(m.to_generic_string)
 							LightModels::Js.add_many_ref_or_att(c,type_name,prop_name,ast_name)
 						elsif m.return_type.array?
 							LightModels::Js.add_many_ref_or_att(c,m.return_type.component_type.name,prop_name,ast_name)
-						elsif m.return_type.enum?
-							has_attr prop_name, String
-						elsif m.return_type.name=='org.mozilla.javascript.Node' or m.return_type.name=='org.mozilla.javascript.ast.AstNode'
+						elsif Js.is_base_class?(m.return_type.name)
 							contains_one_uni prop_name, RGen::MetamodelBuilder::MMBase					
 						else
 							raise "#{ast_name}) Property (single) '#{prop_name}' is else: #{m.return_type}"
