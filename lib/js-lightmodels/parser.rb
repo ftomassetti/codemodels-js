5b7c60fe79e1a81e340ef9c2f8aa891769e79ead
Federico Tomassetti <f.tomassetti@gmail.com>
2013-09-24 11:42:40 +0200

    Calculating position in original source file

--- lib/js-lightmodels/parser.rb
+++ lib/js-lightmodels/parser.rb
@@ -47,11 +47,11 @@ def self.parse_code(code,filename='<code>')
 	reader = StringReader.new(code)
 
 	tree = parser.parse(reader, filename, 1)
-	tree_to_model(tree)
+	tree_to_model(tree,code)
 end
 
-def self.tree_to_model(tree)
-	node_to_model(tree)
+def self.tree_to_model(tree,code)
+	node_to_model(tree,code)
 end
 
 def self.adapter_specific_class(model_class,ref)
@@ -88,15 +88,15 @@ def self.attribute_to_method(model_class,att)
 	s.to_sym
 end
 
-def self.assign_ref_to_model(model,ref,value)
+def self.assign_ref_to_model(model,ref,value,code)
 	return unless value!=nil # we do not need to assign a nil...
 	if ref.many
 		adder_method = :"add#{ref.name.capitalize}"
-		value.each {|el| model.send(adder_method,node_to_model(el))}
+		value.each {|el| model.send(adder_method,node_to_model(el,code))}
 	else
 		setter_method = :"#{ref.name}="
 		raise "Trying to assign an array to a single property. Class #{model.class}, property #{ref.name}" if value.is_a?(::Array)
-		model.send(setter_method,node_to_model(value))
+		model.send(setter_method,node_to_model(value,code))
 	end
 rescue Object => e
 	puts "Problem while assigning ref #{ref.name} (many? #{ref.many}) to #{model.class}. Value: #{value.class}"
@@ -123,7 +123,7 @@ def self.populate_attr(node,att,model)
 	model.send(:"#{att.name}=",value) if value!=nil
 end
 
-def self.populate_ref(node,ref,model)
+def self.populate_ref(node,ref,model,code)
 	value = get_feature_value(node,ref.name,model)
 	if value
 		if value==node
@@ -133,10 +133,10 @@ def self.populate_ref(node,ref,model)
 		if value.is_a?(Java::JavaUtil::Collection)
 			capitalized_name = ref.name.proper_capitalize	
 			value.each do |el|
-				model.send(:"add#{capitalized_name}",node_to_model(el))
+				model.send(:"add#{capitalized_name}",node_to_model(el,code))
 			end
 		else
-			model.send(:"#{ref.name}=",node_to_model(value))
+			model.send(:"#{ref.name}=",node_to_model(value,code))
 		end
 	end
 end
@@ -152,25 +152,48 @@ def self.assign_attr_value(model,prop_name,value)
 	end
 end
 
-def self.assign_ref_value(model,prop_name,value)
+def self.assign_ref_value(model,prop_name,value,code)
 	if value.is_a?(Java::JavaUtil::Collection)
 		capitalized_name = prop_name.proper_capitalize	
 		value.each do |el|
 			#begin
-				model.send(:"add#{capitalized_name}",node_to_model(el))
+				model.send(:"add#{capitalized_name}",node_to_model(el,code))
 			#rescue Object=>e
 			#	raise "Assigning prop #{prop_name} to #{model}: #{e}"
 			#end
 		end
 	else
-		model.send(:"#{ref.name}=",node_to_model(value))
+		model.send(:"#{ref.name}=",node_to_model(value,code))
 	end
 end
 
-def self.node_to_model(node)
+def self.newlines(code,start_pos,end_pos)
+	piece = code[start_pos..end_pos]
+	piece.lines.count
+end
+
+def self.column_last_char(code,start_pos,end_pos)
+	piece = code[start_pos..end_pos]
+	last_line = nil
+	piece.lines.each{|l| last_line=l}
+	last_line.length
+end
+
+def self.node_to_model(node,code)
 	metaclass = get_corresponding_metaclass(node)
 	instance = metaclass.new
+
 	instance.language = LANGUAGE
+	instance.source = LightModels::SourceInfo.new
+	instance.source.begin_pos = LightModels::Position.new
+	instance.source.begin_pos.line = node.lineno
+	instance.source.begin_pos.column = node.position+1
+	instance.source.end_pos = LightModels::Position.new	
+	bp = node.getAbsolutePosition
+	ep = node.getAbsolutePosition+node.length
+	instance.source.end_pos.line = node.lineno+newlines(code,bp,ep)-1
+	instance.source.end_pos.column = column_last_char(code,bp,ep)
+
 	metaclass.ecore.eAllAttributes.each do |attr|
 		unless additional_property?(node.class,attr.name)
 			populate_attr(node,attr,instance)
@@ -178,7 +201,7 @@ def self.node_to_model(node)
 	end
 	metaclass.ecore.eAllReferences.each do |ref|
 		unless additional_property?(node.class,ref.name)
-			populate_ref(node,ref,instance)
+			populate_ref(node,ref,instance,code)
 		end
 	end
 	# check for added properties
@@ -187,7 +210,7 @@ def self.node_to_model(node)
 		if Js.get_att_type(prop_data[:prop_type])
 			assign_attr_value(instance,prop_name.to_s,value)
 		else
-			assign_ref_value(instance,prop_name.to_s,value)
+			assign_ref_value(instance,prop_name.to_s,value,code)
 		end
 	end
 	instance
--- test/test_basic_node_properties.rb
+++ test/test_basic_node_properties.rb
@@ -13,4 +13,26 @@ class TestBasicNodeProperties < Test::Unit::TestCase
 		assert_equal LightModels::Js::LANGUAGE,r.language
 	end	
 
+	def test_node_has_expected_basic_position
+		r = Js.parse_code("i < 10;")
+		assert_not_nil r.source
+		assert_not_nil r.source.begin_pos
+		assert_not_nil r.source.end_pos
+		assert_equal 1,r.source.begin_pos.line
+		assert_equal 1,r.source.begin_pos.column
+		assert_equal 1,r.source.end_pos.line
+		assert_equal 7,r.source.end_pos.column
+	end	
+
+	def test_node_has_expected_multiline_position
+		r = Js.parse_code("{\ni < 10;\n}")
+		assert_not_nil r.source
+		assert_not_nil r.source.begin_pos
+		assert_not_nil r.source.end_pos
+		assert_equal 1,r.source.begin_pos.line
+		assert_equal 1,r.source.begin_pos.column
+		assert_equal 3,r.source.end_pos.line
+		assert_equal 1,r.source.end_pos.column
+	end	
+
 end
